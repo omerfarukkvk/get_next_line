e54c176da56356dff16d0d320127f05cdc88a79b
Omer faruk Kavak <okavak@k1m37s01.42istanbul.com.tr>
2022-08-24 14:23:05 +0300

    updated

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,7 +6,7 @@
 /*   By: okavak <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 18:48:58 by okavak            #+#    #+#             */
-/*   Updated: 2022/06/22 17:44:27 by okavak           ###   ########.fr       */
+/*   Updated: 2022/08/24 14:22:46 by okavak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,9 @@
 
 char	*get_next_line(int fd);
 char	*ft_buff(int fd, char *left_str);
-size_t	ft_strlen(char *str);
+size_t	ft_strlen(const char *str);
 char	*ft_strjoin(char *left_str, char *buff);
-char	*ft_strchr(char *str, int c);
+char	*ft_strchr(const char *str, int c);
 char	*ft_get_line(char *left_str);
 char	*ft_last_str(char *left_str);
 
